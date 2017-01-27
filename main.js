7ddc215746a8fb5a4c6e44d94dcab7660da63789
Brian <brianleediablo3@hotmal.com>
2017-01-27 15:26:40 -0800

    ini

---
+++ main.js
@@ -0,0 +1,8 @@
+var newContent = '<li>three</li>';
+$('.numbers').append(newContent);
+$('.numbers').children(':first').remove();
+$('button').on('click', function () {
+    alert('boooooooooooooooooOOOOOOOOOO');
+});
+
+$('div').append('<button>My Button</button>');
